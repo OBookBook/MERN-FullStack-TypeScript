93d8aa3f75fbb968b8dae111de88d21b002ffcaf
N.I <iworksWeb@gmail.com>
2025-05-12 02:28:43 +0900

    feat MongoDB connect to server/server.ts #1

--- server/server.ts
+++ server/server.ts
@@ -2,8 +2,22 @@ import express, { Express } from "express";
 import userRoute from "./routes/users";
 import authRoute from "./routes/auth";
 import postRoute from "./routes/posts";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Express = express();
+
+// Database
+mongoose
+  .connect(process.env.MONGO_URL!)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch(() => {
+    console.log("db connection failed");
+  });
 
 // Middleware
 app.use("/api/users", userRoute);
