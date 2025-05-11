025bae3384e0f7c46c03a6946d422be6b32c4fd9
N.I <iworksWeb@gmail.com>
2025-05-11 23:39:39 +0900

    add posts route #1

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ const app = express();
 // Middleware
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 // Listening
 const PORT = 3000;
