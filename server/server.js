1c1fb9be9cfe45d7502d96adcb84402e40735c1b
N.I <iworksWeb@gmail.com>
2025-05-11 23:37:33 +0900

    add auth route #1

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const userRoute = require("./routes/users");
-const PORT = 3000;
 
 // Middleware
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 // Listening
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
