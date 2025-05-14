b2998c5ca37432f73d82d532e555f693ee2f7c93
N.I <iworksWeb@gmail.com>
2025-05-14 22:27:37 +0900

    add endpoint POST:http://localhost:3000/api/auth/register #1

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,8 +1,30 @@
-import express, { Router, Request, Response } from "express";
+import express, { Router } from "express";
+import User from "../models/User";
+
 const router: Router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("auth router");
-});
+// POST:http://localhost:3000/api/auth/register
+// body:raw, json
+// {
+//   "username": "testtest",
+//   "email": "test@co.jp",
+//   "password": "testtest"
+// }
+router.post(
+  "/register",
+  async (req: express.Request, res: express.Response): Promise<void> => {
+    try {
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      const user = await newUser.save();
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+);
 
 export default router;
