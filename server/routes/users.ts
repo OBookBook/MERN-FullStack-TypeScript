a2ae365ff739ef811a17725bf93277bad62195bd
N.I <iworksWeb@gmail.com>
2025-05-14 23:03:16 +0900

    add PUT:http://localhost:3000/api/users/:id to server/routes/users.ts #1

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,8 +1,28 @@
-import express, { Router, Request, Response } from "express";
+import express, { Router } from "express";
+import User from "../models/User";
+
 const router: Router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("user router");
+// PUT:http://localhost:3000/api/users/:id
+// example:http://localhost:3000/api/users/682498654d9a38897d7fdf6e
+// body:raw, json
+// {
+//   "userId": "682498654d9a38897d7fdf6e",
+//   "username": "testtest-2"
+// }
+router.put("/:id", async (req: express.Request, res: express.Response) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("updated user");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("you can update only your account");
+  }
 });
 
 export default router;
